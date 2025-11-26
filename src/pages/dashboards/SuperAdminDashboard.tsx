aaaa49e588fd3e119e6ee5094ea73566476e448a
Nischal <tamangnischal2018@gmail.com>
2025-11-26 15:44:11 +0545

    making superadmin dashbaord dynamic

--- src/pages/dashboards/SuperAdminDashboard.tsx
+++ src/pages/dashboards/SuperAdminDashboard.tsx
@@ -551,6 +551,8 @@ const SuperAdminDashboard = () => {
           </>
         );
 
+      case 'all-users':
+        return (
           <>
             <div className="mb-8">
               <h1 className="text-3xl font-bold mb-2">All Users</h1>
